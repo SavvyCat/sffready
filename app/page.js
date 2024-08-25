d095a144fefc087700e702f8c661ea670179265a
Daniel <Theycallmebatman@gmail.com>
2024-08-25 10:04:53 -0700

    Update page.js

--- app/page.js
+++ app/page.js
@@ -156,7 +156,7 @@ export default function Home() {
                     ))}
                   </ul>
                 ) : (
-                  <p class="text-gray-500">Check your GPU dimensions to see a list of compatible cases.</p>
+                  <p className="text-gray-500 text-center">Check your GPU dimensions to see a list of compatible cases.</p>
                 )}
               </div>
             </div>

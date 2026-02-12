2926b42cca90775f1fb0935bcb0a538a72d287d4
Marioo <marioyoussef936@gmail.com>
2026-02-13 01:39:02 +0200

    added sorting in main page

--- Backend/Data/GetDataBasesOn.js
+++ Backend/Data/GetDataBasesOn.js
@@ -14,5 +14,25 @@ export default async function getFilteredCases(gpuLength, gpuHeight, gpuSlots) {
     return [];
   }
 
+  // Fetch volume data from case_details
+  const imageIds = CASES.map((c) => c.image_id);
+  if (imageIds.length > 0) {
+    const { data: details } = await supabase
+      .from("case_details")
+      .select("image_id, volume, product_name")
+      .in("image_id", imageIds);
+
+    if (details) {
+      const volumeMap = {};
+      details.forEach((d) => {
+        volumeMap[d.image_id] = { volume: d.volume, detail_name: d.product_name };
+      });
+      CASES = CASES.map((c) => ({
+        ...c,
+        volume: volumeMap[c.image_id]?.volume || null,
+      }));
+    }
+  }
+
   return CASES;
 }
